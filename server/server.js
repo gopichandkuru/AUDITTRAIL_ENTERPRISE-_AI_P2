81a5fe38017b23f6039b653e5dadf2dd9c60a563
gopichandkuru <gopi76202@gmail.com>
2026-08-27 19:48:25 +0530

    fix(cors): update production CORS origin and render.yaml API URL

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.use(helmet({
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? (process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',') : undefined)
+    ? (process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',') : ['https://audittrail-enterprise-ui.onrender.com'])
     : (origin, callback) => callback(null, true),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
